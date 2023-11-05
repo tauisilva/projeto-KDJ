6b1519896b4cbec5f090379013a75a6f23db5147
Matheus <mgomes494@gmail.com>
2023-11-05 20:12:35 -0300

    fix data path error

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from modules.lsd import loadStockData
+import os
+import pandas as pd
+import yfinance as yf
 
+#  Carregando dados da ação
+def loadStockData(stockTicker):
+    DATA_PATH = "./data/{}.csv".format(stockTicker)
+
+    if os.path.exists(DATA_PATH): 
+      with open(DATA_PATH) as file:
+        stock_history = pd.read_csv(DATA_PATH)
+    else:
+        stock = yf.Ticker(stockTicker)
+        stock_history = stock.history(period="max")
+        stock_history.to_csv(DATA_PATH)
+    
+    return stock_history
+
+
+# 
 appleStockHistory = loadStockData("aapl")
 print(appleStockHistory.tail(5)) # Últimos 5 dias de negociação
